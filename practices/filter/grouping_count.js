d36094e63846ef884823a67aea78065ab0ef6034
index.html <32197390+Gaozebin@users.noreply.github.com>
2017-12-28 18:53:57 +0800

    Update grouping_count.js

--- practices/filter/grouping_count.js
+++ practices/filter/grouping_count.js
@@ -3,6 +3,15 @@
 function grouping_count(collection) {
 
   //在这里写入代码
+  
+   function getWordCnt(){
+	    return collection.reduce(function(prev,next){
+	        prev[next] = (prev[next] + 1) || 1;
+	        return prev;
+	    },{});
+	  }
+	 
+	return getWordCnt();
 }
 
 module.exports = grouping_count;
