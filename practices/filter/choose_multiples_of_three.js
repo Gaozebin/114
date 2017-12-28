b54490ee3809467953257ba80d0023e457c20fa0
index.html <32197390+Gaozebin@users.noreply.github.com>
2017-12-28 18:51:51 +0800

    Update choose_multiples_of_three.js

--- practices/filter/choose_multiples_of_three.js
+++ practices/filter/choose_multiples_of_three.js
@@ -3,6 +3,15 @@
 function choose_multiples_of_three(collection) {
 
   //在这里写入代码
+  var result = collection.filter(function(item){
+	  	if(item % 3 == 0){
+	  		return item; // 
+	  	}
+  });
+  result.push(0);
+  result.sort();
+  return result;
+  
 }
 
 module.exports = choose_multiples_of_three;
