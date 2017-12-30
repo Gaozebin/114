bcb4603c4d8c0e17814748968708e8ebf950daf9
index.html <32197390+Gaozebin@users.noreply.github.com>
2017-12-30 23:10:53 +0800

    Update average_uneven.js

--- practices/superposition_operation/interval_operation/average_uneven.js
+++ practices/superposition_operation/interval_operation/average_uneven.js
@@ -3,6 +3,14 @@
 function average_uneven(collection) {
 
   //在这里写入代码
+   var sum = 0, count = 0;
+    collection.forEach(function (value) {
+        if(value % 2 != 0){
+            count ++;
+            sum += value;
+        }
+    });
+    return sum / count;
 }
 
 module.exports = average_uneven;
