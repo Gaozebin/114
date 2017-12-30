33bbac8c2f2e5b5d9a9d035d0b94d35219e710ae
index.html <32197390+Gaozebin@users.noreply.github.com>
2017-12-30 23:06:36 +0800

    Update letter_mapping_2.js

--- practices/superposition_operation/collection_operation/letter_mapping_2.js
+++ practices/superposition_operation/collection_operation/letter_mapping_2.js
@@ -3,6 +3,10 @@
 function average_to_letter(collection) {
 
   //在这里写入代码
+   var result = collection.reduce(function(a,b){
+  	return a+b;
+  });
+  return String.fromCharCode(96 + Math.ceil(result/collection.length));
 }
 
 module.exports = average_to_letter;
