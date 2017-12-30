669a28cd89e62804a56f83927aa5cc6cb3d00c4f
index.html <32197390+Gaozebin@users.noreply.github.com>
2017-12-30 22:42:17 +0800

    Update double_dimensional_array_to_one_1.js

--- practices/flaten/double_dimensional_array_to_one_1.js
+++ practices/flaten/double_dimensional_array_to_one_1.js
@@ -3,6 +3,10 @@
 function double_to_one(collection) {
 
   //在这里写入代码
+ return collection.join().split(",")
+  		 .map(function(item){
+  		 	return parseInt(item);
+  		 });
 }
 
 module.exports = double_to_one;
