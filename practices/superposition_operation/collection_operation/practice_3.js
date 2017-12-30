54656e8903e6c92023789a6fd77a1592ebe0a65f
index.html <32197390+Gaozebin@users.noreply.github.com>
2017-12-30 23:08:35 +0800

    Update practice_3.js

--- practices/superposition_operation/collection_operation/practice_3.js
+++ practices/superposition_operation/collection_operation/practice_3.js
@@ -3,6 +3,13 @@
 function hybrid_operation_to_uneven(collection) {
 
   //在这里写入代码
+  var sum = 0;
+    collection.forEach(function (value) {
+        if(value % 2 != 0){
+            sum += value * 3 + 5;
+        }
+    });
+    return sum;
 }
 
 module.exports = hybrid_operation_to_uneven;
