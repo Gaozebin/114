c50b8e063e85eae0b45ab96a7c5ed3c14a4a02ee
index.html <32197390+Gaozebin@users.noreply.github.com>
2017-12-30 22:51:52 +0800

    Update compute_chain_median.js

--- practices/reduce/compute_chain_median.js
+++ practices/reduce/compute_chain_median.js
@@ -2,6 +2,19 @@
 
 function compute_chain_median(collection) {
   //在这里写入代码
+  var result = collection.split("->")
+  						.map(function(item){
+  							return parseInt(item);
+   				}).sort(function(a,b){
+   					return a - b;
+   				});
+  // console.log(result);
+  if(result.length % 2 == 0){
+  	  return (result[result.length / 2] + result[result.length/2 -1])/2;
+  } else{
+  	return (result[result.length/2]);
+  }
+
 }
 
 module.exports = compute_chain_median;
